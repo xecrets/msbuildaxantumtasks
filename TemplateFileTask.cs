1517bdb9024831620e00ad8f3c46557b7dac68d2
Svante Seleborg <svante@axantum.com>
2014-04-19 12:51:08 +0200

    Add TemplateFileTask

---
+++ TemplateFileTask.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace MSBuild.Axantum.Tasks
+{
+    public class TemplateFileTask : Task
+    {
+        [Required]
+        public string TemplateFile { get; set; }
+
+        [Required]
+        public string TargetFile { get; set; }
+
+        [Required]
+        public ITaskItem[] Values { get; set; }
+
+        public override bool Execute()
+        {
+            string templateContent = File.ReadAllText(TemplateFile);
+
+            foreach (ITaskItem item in Values)
+            {
+                templateContent = templateContent.Replace(item.ItemSpec, item.GetMetadata("Value"));
+            }
+
+            if (File.Exists(TargetFile) && File.ReadAllText(TargetFile) == templateContent)
+            {
+                return true;
+            }
+
+            File.WriteAllText(TargetFile, templateContent);
+            return true;
+        }
+    }
+}
